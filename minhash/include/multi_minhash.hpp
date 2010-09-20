368708579e3582a5882581cc291be2f88883d4f9
ohtaman <ohtamans@gmail.com>
2010-09-20 23:58:01 +0900

    done something...

--- minhash/include/multi_minhash.hpp
+++ minhash/include/multi_minhash.hpp
@@ -3,7 +3,7 @@
  * @author Ohtaman
  * @brief
  *
- * @date Mon Sep 20 16:08:59 2010 last updated
+ * @date Mon Sep 20 23:38:26 2010 last updated
  * @date Mon Sep 20 10:35:43 2010 created
  */
 
@@ -106,11 +106,95 @@ namespace colfil {
         (*output)[i] = minHashes_[i](input);
       }
     }
+  };
 
-    void operator()(const ContainerType &input, MinHashValueType *output)
+
+  template<unsigned int N,
+           typename INPUT_VALUE_T,
+           typename MIN_HASH = MinHash<const INPUT_VALUE_T*>,
+           typename VALUE_T = Array<typename MIN_HASH::ValueType, N>,
+           typename RANDOM_T = NLRandom>
+  class MultiMinHash : public Hash<typename MIN_HASH::InputType, VALUE_T>{
+  public:
+
+    typedef CONTAINER_T ContainerType;
+    typedef MIN_HASH MinHashType;
+    typedef typename MinHashType::ValueType MinHashValueType;
+    typedef MultiMinHash<N, CONTAINER_T, MIN_HASH, VALUE_T, RANDOM_T> SelfType;
+    typedef VALUE_T ValueType;
+    typedef RANDOM_T RandomType;
+    typedef typename RandomType::SeedType SeedType;
+
+  private:
+
+    RandomType random_;
+    MinHashType minHashes_[N];
+
+    void setSeeds()
     {
       for (unsigned int i = 0; i < N; ++i) {
-        *output[i] = minHashes_[i](input);
+        minHashes_[i].setSeed(random_());
+      }
+    }
+
+  public:
+
+    MultiMinHash()
+    {
+      setSeeds();
+    }
+
+    MultiMinHash(int seed)
+    {
+      random_.setSeed(seed);
+      setSeeds();
+    }
+
+    virtual ~MultiMinHash()
+    {
+    }
+
+    MinHashType &getHash(unsigned int i)
+    {
+      return minHashes_[i];
+    }
+
+    const MinHashType &getHash(unsigned int i) const
+    {
+      return minHashes_[i];
+    }
+
+    unsigned int size() const
+    {
+      return N;
+    }
+
+    void setSeed(int seed)
+    {
+      random_.setSeed(seed);
+      setSeeds();
+    }
+
+    int getSeed() const
+    {
+      return random_.getSeed();
+    }
+
+    ValueType operator()(const ContainerType &input) const
+    {
+      ValueType output;
+
+      for (unsigned int i = 0; i < N; ++i) {
+        output[i] = minHashes_[i](input);
+      }
+
+      return output;
+    }
+
+    void operator()(const ContainerType &input, ValueType *output)
+    {
+      for (unsigned int i = 0; i < N; ++i) {
+        (*output)[i] = minHashes_[i](input);
       }
     }
   };
